f9cfbc30ee716a334c2e6c99f7df80875f727211
barreiradlc ( Augusto Barreira ) <barreira266@hotmail.com>
2024-07-05 20:10:44 -0300

    Init project

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,34 @@
+import { Text, TextInput, TouchableOpacity, View } from "react-native"
+import { styles } from "./styles"
+
+function Home() {
+  function handleParticipantAdd() {
+    console.debug("ADD!")
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do evento</Text>
+      <Text style={styles.eventDate}>Sext, 4 de Novembro de 2022.</Text>
+
+      <View style={styles.form}>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Nome do participante"
+          placeholderTextColor="#6B6B6"
+        />
+
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleParticipantAdd}
+        >
+          <Text style={styles.buttonText}> + </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+}
+
+
+export { Home }
